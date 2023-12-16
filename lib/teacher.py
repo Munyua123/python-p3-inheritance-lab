02037159930a5350aa3b36bc5b7fee1db13c3e48
Earl <earlmunyua96@gmail.com>
2023-12-16 11:38:40 +0300

    inhetit

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,23 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+        self.knowledge = knowledge if knowledge is not None else []
 
     def teach(self):
-        pass
+        if self.knowledge:
+            return random.choice(self.knowledge)
+        else:
+            return "I have no knowledge to share right now."
+
